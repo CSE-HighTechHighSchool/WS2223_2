ec893c6982b18b40b49e8a228210597d83321887
spoutshield <shojha@ctemc.org>
2022-10-19 09:02:22 -0400

    home commit animations 10/19
    added fade left and fade right animations, home page is done! we are absolutely cracked

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const fadeUp = new IntersectionObserver(
         })
     }, 
     {
-        threshold: 0.8,
+        threshold: 0.5,
     }
 )
 
@@ -16,3 +16,39 @@ invisUp.forEach(invis => {
     fadeUp.observe(invis)
 })
 
+const invisRight = document.querySelectorAll(".fadesRight")
+
+const fadeRight = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("fade-right", entry.isIntersecting)
+            if (entry.isIntersecting) fadeRight.unobserve(entry.target)
+        })
+    }, 
+    {
+        threshold: 0.5,
+    }
+)
+
+invisRight.forEach(invis => {
+    fadeRight.observe(invis)
+})
+
+
+const invisLeft = document.querySelectorAll(".fadesLeft")
+
+const fadeLeft = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("fade-left", entry.isIntersecting)
+            if (entry.isIntersecting) fadeLeft.unobserve(entry.target)
+        })
+    }, 
+    {
+        threshold: 0.5,
+    }
+)
+
+invisLeft.forEach(invis => {
+    fadeLeft.observe(invis)
+})
