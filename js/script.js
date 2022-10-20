90ed41820bf7754e0898c0fbcd7d17e5e3a82f28
spoutshield <shojha@ctemc.org>
2022-10-19 20:16:07 -0400

    css and js comments 10/19
    added comments for css and js. but the worst is yet to come...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* 
+    The first intersection observer is used to observe things that fade up. When 50% of an element with the fades class enters the screen, it will fade up through the fade-in class.
+**/
+
 const invisUp = document.querySelectorAll(".fades")
 
 const fadeUp = new IntersectionObserver(
@@ -15,6 +19,11 @@ const fadeUp = new IntersectionObserver(
 invisUp.forEach(invis => {
     fadeUp.observe(invis)
 })
+
+
+/* 
+    The second intersection observer is used to observe things that fade to the right. When 50% of an element with the fadesRight class enters the screen, it will fade to the right with the fade-right class.
+**/
 
 const invisRight = document.querySelectorAll(".fadesRight")
 
@@ -35,6 +44,10 @@ invisRight.forEach(invis => {
 })
 
 
+/* 
+    The third intersection observer is used to observe things that fade to the left. When 50% of an element with the fadesLeft class enters the screen, it will fade to the left with the fade-left class.
+**/
+
 const invisLeft = document.querySelectorAll(".fadesLeft")
 
 const fadeLeft = new IntersectionObserver(
@@ -52,6 +65,11 @@ const fadeLeft = new IntersectionObserver(
 invisLeft.forEach(invis => {
     fadeLeft.observe(invis)
 })
+
+
+/* 
+    The final intersection observer is used to observe the navbar. When 100% of the background image is seen on screen, the navbar must be at the very top of the page. The function then takes away any existing background color, and adds a post-animation if there was background color to begin with. If 100% of the background image is not on sreen, it will add a background color class, making the navbar brown.
+**/
 
 const background = document.querySelector(".top-img")
 const nb = document.querySelector(".nb-stuff")
