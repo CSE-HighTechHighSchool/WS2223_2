0406e6702a5553e44d9be61019fa274cc2de44eb
spoutshield <shojha@ctemc.org>
2023-01-09 09:44:06 -0500

    alert changes 1/9
    added an animation

--- js/alert.js
+++ js/alert.js
@@ -1,7 +1,7 @@
 export function bootstrapAlert(message, type) {
     var alert = document.createElement('div');
     alert.setAttribute('role', 'alert');
-    alert.classList.add('alert', 'strong', 'alert-dismissible', 'show', 'alert-' + type, 'fade');
+    alert.classList.add('alert', 'strong', 'alert-dismissible', 'show', 'alert-' + type, 'fade', 'fades', 'fade-in');
     alert.innerHTML = message;
 
     var button = document.createElement('button');
