5789e2b1fa3e06683055019fbb774358aae72110
squareman77 <75134215+squareman77@users.noreply.github.com>
2023-01-11 09:04:55 -0500

    Update account

---
+++ js/account.js
@@ -0,0 +1,171 @@
+// ----------------- Page Loaded After User Sign-in -------------------------//
+
+// ----------------- Firebase Setup & Initialization ------------------------//
+import { bootstrapAlert } from './alert.js';  
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {getAuth, signInWithEmailAndPassword, updateEmail} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import {getDatabase, ref, set, update, child, get, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCL58GYKI97YFdpJseNOnUmDpp4YPp1dCQ",
+  authDomain: "fusion-360-cuisine.firebaseapp.com",
+  projectId: "fusion-360-cuisine",
+  storageBucket: "fusion-360-cuisine.appspot.com",
+  messagingSenderId: "477691509900",
+  appId: "1:477691509900:web:5aa146a0ab8f38cd484fa9",
+  measurementId: "G-6MG2F9352K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Auth
+const auth = getAuth();
+
+// Initialize Database
+const db = getDatabase(app);
+
+// ---------------------// Get reference values -----------------------------
+
+let signOutLink = document.getElementById("signOut");
+let welcome_message = document.getElementById("welcome-message");
+let orderLink = document.getElementById("orderLink");
+let end = document.getElementById("end");
+let currentUser = null;
+
+// ----------------------- Get User's Name'Name ------------------------------
+
+function getUsername() {
+// Get the user's name from storage
+let keepLoggedIn = localStorage.getItem("keepLoggedInSwitch");
+// If the user's name is not in storage, get it from session storage
+if (keepLoggedIn == "yes") {
+  currentUser = JSON.parse(localStorage.getItem('user'))
+} else {
+  currentUser = JSON.parse(sessionStorage.getItem('user'))
+}
+
+if (currentUser != null) currentUser = currentUser.accountInfo;
+}
+
+// Sign-out function that will remove user info from local/session storage and
+// sign-out from FRD
+
+function signOutUser() {
+// Remove user info from local/session storage
+localStorage.removeItem("user");
+sessionStorage.removeItem("user");
+// Sign-out from FRD
+auth.signOut().then(() => {
+  // Sign-out successful.
+  window.location.href = "index.html";
+}).catch((error) => {
+  // An error happened.
+  console.log(error);
+})}
+
+window.onload = window.addEventListener("load",navload(),false);
+
+export function navload () {
+getUsername();
+
+if (currentUser == null) {
+
+  signOutLink.innerText = "Sign In";
+  signOutLink.href = "signIn.html";
+  signOutLink.classList.add("strong");
+  orderLink.remove();
+
+} else {
+  console.log(currentUser);
+  console.log(currentUser.uid)
+  signOutLink.innerText = "Sign Out";
+  signOutLink.classList.add("strong")
+  signOutLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    signOutUser();
+  });
+  
+  let adminLink = document.createElement("a");
+  adminLink.setAttribute("id", "adminLink");
+  adminLink.classList.add("nav-link");
+  adminLink.classList.add("hover-underline-animation");
+  adminLink.classList.add("strong");
+
+  end.appendChild(adminLink);
+  
+
+  if (currentUser.isAdmin) {
+    adminLink.setAttribute("href", "admin.html");
+    adminLink.innerText = "Admin Console";
+  } else {
+    adminLink.innerText = "My Account"
+    adminLink.setAttribute("href", "myacc.html")
+  }
+  console.log(window.location.href)
+  if (window.location.href.match("admin.html") != null) {
+    adminLink.classList.add("active");
+  } else if (window.location.href.match("myacc.html") != null) {
+    adminLink.classList.add("active");
+    welcome_message.innerHTML = "Welcome, " + currentUser.firstName + ".";
+    //insert code here
+    
+    end.classList.add("active");
+  }
+}
+};
+
+
+function updateAccountInfo(name, email, userID) {
+    // Set the data
+    
+    const nameArr = name.split(" ");
+    const firstName = nameArr[0];
+    const lastName = nameArr.slice(1).join(' ');
+
+    update(ref(db, `users/${userID}/accountInfo`), {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+    })
+    .then(() => {
+        alert("Data updated successfully");
+        window.location.href = "myacc.html";
+    })
+    .catch((error) => {
+        alert(`Error: ${error.code} - ${error.message}`);
+    });
+
+    updateEmail(auth.currentUser, email).then(() => {
+        // Email updated!
+        // ...
+    }).catch((error) => {
+        // An error occurred
+        // ...
+    });
+}
+
+document.getElementById('submitAccountInfo').addEventListener("click", (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name_input').value; 
+    const email = document.getElementById('email_input').value; 
+    getUsername();
+
+    if (currentUser == null) {
+
+
+    } else {
+        console.log(currentUser.uid)
+        updateAccountInfo(name, email, currentUser.uid)
+    }
+
+})
